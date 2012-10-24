232c2fc42676787a8463bbe68bd0d2b1c9c0cd3e
Itamar Bar-Lev <itamar@gizra.com>
2012-10-24 14:08:34 +0200

    Inventory updates.

--- opengizra/modules/custom/opengizra_inventory/plugins/content_types/summary/inventory-cell.tpl.php
+++ opengizra/modules/custom/opengizra_inventory/plugins/content_types/summary/inventory-cell.tpl.php
@@ -1,6 +1,9 @@
 <div class="quantity"><?php print $quantity; ?></div>
-<?php if ($availability): ?>
-  <div class="availability <?php print $class; ?>">
-    <span class="amount"><?php print $availability; ?></span><span class="label"><?php print $label; ?></span>
-  </div>
-<?php endif; ?>
+
+<?php foreach($subfigure_types as $type => $label): ?>
+  <?php if ($$type): ?>
+    <div class="subfigure <?php print $type; ?>">
+      <span class="amount"><?php print $$type; ?></span><span class="label"><?php print $label; ?></span>
+    </div>
+  <?php endif; ?>
+<?php endforeach; ?>
