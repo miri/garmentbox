62b3a284294d0a7843db94a21980b4501ea3182e
Itamar Bar-Lev <itamar@gizra.com>
2012-10-28 15:47:59 +0200

    Updating variant tests.

--- opengizra/behat/features/bootstrap/FeatureContext.php
+++ opengizra/behat/features/bootstrap/FeatureContext.php
@@ -2,6 +2,7 @@
 
 use Drupal\DrupalExtension\Context\DrupalContext;
 use Behat\Behat\Context\Step\Given;
+use Behat\Gherkin\Node\TableNode;
 
 require 'vendor/autoload.php';
 
@@ -12,8 +13,84 @@ class FeatureContext extends DrupalContext {
    */
   public function iAmOnAPageWithId($arg1, $arg2) {
     $path = 'node/' . $arg2;
-    echo $path;
+
     // Use Drupal Context 'I am at'.
     return new Given("I am at \"$path\"");
+  }
+
+  /**
+   * @Given /^I should see the following <links>$/
+   */
+  public function iShouldSeeTheFollowingLinks(TableNode $table) {
+    $page = $this->getSession()->getPage();
+    $table = $table->getHash();
+
+    foreach ($table as $key => $value) {
+      $link = $table[$key]['links'];
+      $result = $page->findLink($link);
+      if(empty($result)) {
+        throw new Exception("The link '" . $link . "' was not found");
+      }
+    }
+  }
+
+  /**
+   * @Then /^I should see a table titled "([^"]*)" with the following <contents>:$/
+   */
+  public function iShouldSeeATableTitledWithTheFollowingContents($title, TableNode $table) {
+    $page = $this->getSession()->getPage();
+    // Find the container of the table with the correct pane title
+    $element = $page->find('xpath', '//h2[.="' . $title .'"]/parent::div');
+    if (!$element) {
+      throw new Exception("No pane titled '$title' was found.");
+    }
+    $element = $element->find('css', 'table');
+    if (!$element) {
+      throw new Exception("No table was found inside the pane titled '$title'.");
+    }
+
+    // Compare the table header.
+    $this->compareTableRow($element->findAll('css', 'thead th'), $table->getRow(0));
+
+    // Compare the rows.
+    foreach ($element->findAll('css', 'tbody tr') as $i => $row) {
+       $this->compareTableRow($row->findAll('css', 'td'), $table->getRow($i + 1));
+    }
+  }
+
+  /**
+   * @Given /^the BOM total should be "([^"]*)"$/
+   */
+  public function theBomTotalShouldBe($total) {
+    $element = $this->getSession()->getPage();
+    if ($element->find('css', '.bom-total .amount')->getText() != $total) {
+      throw new Exception("The BOM has a different total price than '$total'");
+    }
+  }
+
+  /**
+   * Compare a present table row cells with the expected row.
+   *
+   * @param $cells
+   *   Array of table row cells retrieved with findAll().
+   * @param $expected_row
+   *   One row from the TableNode object.
+   */
+  private function compareTableRow($cells, $expected_row) {
+    foreach ($cells as $i => $cell) {
+      if (empty($expected_row[$i])) {
+        throw new Exception("Unexpected cell with text '{$th->getText()}'.");
+        continue;
+      }
+
+      if ($cell->getText() != $expected_row[$i]) {
+        throw new Exception("Found '{$cell->getText()}' instead of '{$expected_row[$i]}'.");
+      }
+    }
+
+    if (count($expected_row) > $i) {
+      throw new Exception('Missing column.');
+    }
+
   }
 }
--- opengizra/behat/features/item_variant.feature
+++ opengizra/behat/features/item_variant.feature
@@ -3,24 +3,30 @@ Feature: Test item variant page
   presented correctly.
 
   @api
+  Scenario: Verify that the basic information is shown correctly.
+    Given I am logged in as a user with the "authenticated user" role
+    And I am on a "item-variant" page with id "37"
+    Then I should see the heading "Pink Vest dress"
+
+  @api
   Scenario: Test the tabs of an item variant.
     Given I am logged in as a user with the "authenticated user" role
     And I am on a "item-variant" page with id "37"
     Then I should see the following <links>
-    | Main                  |
-    | Pink Vest dress       |
-    | Salmon Vest dress     |
-    | Peach Vest dreslinks  |
+    | links             |
+    | Main              |
+    | Pink Vest dress   |
+    | Salmon Vest dress |
+    | Peach Vest dress  |
 
-  @api
+  @api @wip
   Scenario: Test the prices on the BOM table.
     Given I am logged in as a user with the "authenticated user" role
     And I am on a "item-variant" page with id "37"
-    Then I should see a table titled "Bill of materials":
-    | Quantity  | Material item   | Unit  | Price |
-    | 2.50      | [Vogue Fabrics] | Meter | $5.00 |
-    And I shoud see the text "Total: $5.00"
-
+    Then I should see a table titled "Bill of materials" with the following <contents>:
+    | Quantity  | Material item                                             | Unit  | Price |
+    | 2.50      | Novelle Art Noveau Natural Shell Buttons [Vogue Fabrics]  | Meter | $5.00 |
+    And the BOM total should be "$5.00"
 
   @api
   Scenario: Test the prices on the BOL table.
@@ -35,7 +41,7 @@ Feature: Test item variant page
   Scenario: Test the total production price.
     Given I am logged in as a user with the "authenticated user" role
     And I am on a "item-variant" page with id "37"
-    Then I shoud see the text "production price: $25.00"
+    Then I should see the text "production price: $25.00"
 
   @api
   Scenario:
