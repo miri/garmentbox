b757798e236912ab5e0bee52cb0b4340f3af7905
Itamar Bar-Lev <itamar@gizra.com>
2012-10-30 17:19:56 +0200

    Trying to set up default site.

--- opengizra/behat/aliases.drushrc.php
+++ opengizra/behat/aliases.drushrc.php
@@ -1,7 +1,7 @@
 <?php
 
 $aliases['opengizra'] = array(
-  'uri' => 'http://127.0.0.1/www/',
-  'root' => '~/builds/Gizra/Garment-Box/www',
+  'uri' => 'http://127.0.0.1/',
+  'root' => '/home/travis/builds/Gizra/Garment-Box/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
