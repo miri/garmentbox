eee85414a13918861d5d7bbbf0ac62269d5f1668
Itamar Bar-Lev <itamar@gizra.com>
2012-10-30 15:12:06 +0200

    Fixing root URL.

--- opengizra/behat/aliases.drushrc.php
+++ opengizra/behat/aliases.drushrc.php
@@ -2,6 +2,6 @@
 
 $aliases['opengizra'] = array(
   'uri' => '127.0.0.1',
-  'root' => '/home/travis/builds/Gizra/Garment-Box/www',
+  'root' => '~/builds/Gizra/Garment-Box/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
