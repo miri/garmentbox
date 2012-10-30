4c2137788093fd92ea7e0b9ea8fbb85d89482a7a
Itamar Bar-Lev <itamar@gizra.com>
2012-10-30 14:45:29 +0200

    Trying to fix alias.

--- opengizra/behat/aliases.drushrc.php
+++ opengizra/behat/aliases.drushrc.php
@@ -2,6 +2,6 @@
 
 $aliases['opengizra'] = array(
   'uri' => '127.0.0.1',
-  'root' => '/home/travis/builds/sprice/drupal',
+  'root' => '/home/travis/builds/sprice/drupal/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
