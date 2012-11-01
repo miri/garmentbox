61b1b1384f68a8f4ca44b30b5fdb7e031beed40c
Itamar Bar-Lev <itamar@gizra.com>
2012-11-01 16:39:59 +0200

    renaming.

--- garmentbox/modules/custom/garmentbox_bubble/js/garmentbox_bubble.js
+++ garmentbox/modules/custom/garmentbox_bubble/js/garmentbox_bubble.js
@@ -3,8 +3,8 @@
 Drupal.behaviors.garmentBoxBubble = {
   attach: function(context) {
     // Open bubbles that are marked as "Open".
-    for (var i in Drupal.settings.garment_box_bubble.opened_selectors) {
-      $(Drupal.settings.garment_box_bubble.opened_selectors[i]).btOn();
+    for (var i in Drupal.settings.garmentbox_bubble.opened_selectors) {
+      $(Drupal.settings.garmentbox_bubble.opened_selectors[i]).btOn();
     }
   }
 };
