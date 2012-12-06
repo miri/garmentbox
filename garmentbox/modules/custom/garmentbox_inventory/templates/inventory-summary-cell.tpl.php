274c0d71441ad4a094f3acf9f5054e0ebc5f002f
Amir Arbel <amir@gizra.com>
2012-12-06 13:52:37 +0200

    Hide empty subfiguers.

--- garmentbox/modules/custom/garmentbox_inventory/templates/inventory-summary-cell.tpl.php
+++ garmentbox/modules/custom/garmentbox_inventory/templates/inventory-summary-cell.tpl.php
@@ -4,15 +4,17 @@
     <div class="label"><?php print t('available'); ?></div>
   </div>
 
-  <div class="sub-figures">
-    <?php foreach($subfigure_types as $type => $label): ?>
-        <div class="sub-figure <?php print $type; ?>">
-          <?php if ($$type): ?>
-            <span class="amount"><?php print $$type; ?></span><span class="label"><?php print $label; ?></span>
-          <?php else: ?>
-            <span class="sub-figure-placeholder"></span>
-          <?php endif; ?>
-        </div>
-    <?php endforeach; ?>
-  </div>
+  <?php if ($has_subfigures): ?>
+    <div class="sub-figures">
+      <?php foreach($subfigure_types as $type => $label): ?>
+          <div class="sub-figure <?php print $type; ?>">
+            <?php if ($$type): ?>
+              <span class="amount"><?php print $$type; ?></span><span class="label"><?php print $label; ?></span>
+            <?php else: ?>
+              <span class="sub-figure-placeholder"></span>
+            <?php endif; ?>
+          </div>
+      <?php endforeach; ?>
+    </div>
+  <?php endif; ?>
 </div>
