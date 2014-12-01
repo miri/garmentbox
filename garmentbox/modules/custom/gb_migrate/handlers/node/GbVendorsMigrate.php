31eedb829f4e5580f8b773409eeafeb64ba0bed4
Miri <miri.meltzer1@gmail.com>
2014-12-01 15:30:50 +0200

    Add Vendor content type

--- garmentbox/modules/custom/gb_migrate/handlers/node/GbVendorsMigrate.php
+++ garmentbox/modules/custom/gb_migrate/handlers/node/GbVendorsMigrate.php
@@ -10,18 +10,21 @@ class GbVendorsMigrate extends GbMigration {
   public $entityType = 'node';
   public $bundle = 'vendor';
 
-  public $csvColumns = array(
-    array('title', 'Title'),
-  );
+//  public $csvColumns = array(
+//    array('title', 'Title'),
+//  );
 
-  public function __construct() {
-    parent::__construct();
+//  public function __construct() {
+//    parent::__construct();
+//
+//    $this->addFieldMapping('title', 'title');
+//
+//
+//  }
 
-    $this->addFieldMapping('title', 'title');
-
-    $this
-      ->addFieldMapping('field_vendor', 'field_vendor')
-      ->sourceMigration('GbVendorsMigrate');
-  }
+//public function prepareRow() {
+//    parent::prepareRow();
+//    dpm(func_get_args());
+//}
 
 }
